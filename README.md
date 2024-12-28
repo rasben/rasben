cc618b7dd671dc12ef911f626b51f543c81f505b
Benjamin Rasmussen <rasben@users.noreply.github.com>
2024-12-28 23:35:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <img alt="fandango" src="https://raw.githubusercontent.com/rasben/rasben/refs/heads/main/static/fandango.gif" />
+</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<hgroup>
+	<h1><strong>ben</strong>jamin <strong>ras</strong>mussen</h1>
+</hgroup>
+
+<p>
+	senior developer at <a href="https://reload.dk">reload.dk</a>,
+	<a href="https://dev.acquia.com/public/registry?title=benjamin+rasmussen"
+		>certified drupal specialist</a
+	>
+	and obsessed with <a href="https://youtu.be/6PIGPUem-Jk">obscure internet references</a>
+</p>
+
+<nav aria-label="socials">
+	<ul>
+		<li>
+			<strong>github</strong>
+			<a href="https://github.com/rasben" title="github">rasben</a>
+		</li>
+		<li>
+			<strong>drupal</strong>
+			<a href="https://www.drupal.org/u/ras-ben" title="drupal.org">/u/ras-ben</a>
+		</li>
+		<li>
+			<strong>linkedin</strong>
+			<a href="https://linkedin.com/in/rasben" title="linkedin">/in/rasben</a>
+		</li>
+		<li>
+			<strong>bluesky</strong>
+			<a href="https://bsky.app/profile/rasben.net">@rasben.net</a>
+		</li>
+	</ul>
+</nav>
